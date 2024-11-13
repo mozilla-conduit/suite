774c9028e59b9512d01e9517bc49f2272b97f55e
Olivier Mehani <omehani@mozilla.com>
2024-11-13 15:46:28 +1100

    gogs: manually restore data (bug 1864902)
    This is a work around for [0]
    
    [0] https://github.com/gogs/gogs/issues/7840

--- docker/gogs/scripts/utils.sh
+++ docker/gogs/scripts/utils.sh
@@ -37,6 +37,18 @@ gogs_restore()
 {
   mkdir -p "${RESTORE_TMPDIR}"
   chown "${USER}" "${RESTORE_TMPDIR}"
-  gosu "${USER}" ./gogs restore --tempdir "${RESTORE_TMPDIR}" --from ${BACKUP_FILE}
+  # Workaround [0]: restore DB, then manually extract repositories
+  # [0] https://github.com/gogs/gogs/issues/7840
+  gosu "${USER}" ./gogs restore --tempdir "${RESTORE_TMPDIR}" --from ${BACKUP_FILE} --database-only
 
+  # shellcheck disable=SC2164
+  cd "${RESTORE_TMPDIR}"
+  unzip ${BACKUP_FILE} gogs-backup/repositories.zip
+
+  # shellcheck disable=SC2164
+  cd /data/git
+  unzip "${RESTORE_TMPDIR}/gogs-backup/repositories.zip"
+  chown -R "${USER}" .
+
+  rm -rf "${RESTORE_TMPDIR}"
 }
