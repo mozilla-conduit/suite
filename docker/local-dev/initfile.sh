4c12d4c40a509d598a2dee6badd0f753355c9473
Olivier Mehani <omehani@mozilla.com>
2025-08-14 17:36:59 +1000

    local-dev: typo in init script

--- docker/local-dev/initfile.sh
+++ docker/local-dev/initfile.sh
@@ -2,9 +2,9 @@
 # Don't use a .bashrc so that, if the entrypoint needs to be skipped, you can just override CMD to be "bash" (rather
 # than having to force bash to ignore the .bashrc)
 if [ -f ~/.volume-initialized ]; then
-  echo "Repos already initialized...\n"
+  echo "Repos already initialized..."
 else
-  echo "Initializing repos...\n"
+  echo "Initializing repos..."
   ./clone_repositories.sh
   touch ~/.volume-initialized
 fi
